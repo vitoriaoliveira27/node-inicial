207635f02906183e60dd6cd5d2d06859a638cfe9
Vitoria <vitoriahta27@gmail.com>
2024-10-13 17:09:52 -0300

    feat: :sparkles: add: modelo eventos e teste do modelo

---
+++ src/test/models/evento.test.js
@@ -0,0 +1,22 @@
+/* eslint-disable linebreak-style */
+import {
+  describe, expect, it,
+} from '@jest/globals';
+import Evento from '../../models/evento.js';
+
+describe('Testando o modelo Evento', () => {
+  const objetoEvento = {
+    nome: 'Evento teste',
+    descricao: 'Descricao do evento teste',
+    data: '2023-01-01',
+    autor_id: 46,
+  };
+
+  it('Deve instanciar um novo evento', () => {
+    const evento = new Evento(objetoEvento);
+
+    expect(evento).toEqual(
+      expect.objectContaining(objetoEvento),
+    );
+  });
+});
